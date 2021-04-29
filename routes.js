5294992adc01c325dd4ff216c200a148cab499bb
giovanniandr <fs_giovanni@hotmail.com>
2021-04-29 09:06:39 +0000

    test

--- routes.js
+++ routes.js
@@ -1,12 +1,15 @@
 const express = require('express'),
 router = express.Router();
 
-router.get('/hello/:foo/:bar', function(req, res) {
-    res.json({message: 'Hello JSON', data: [
-        req.params.foo,
-        req.params.bar
-    ] });
-});
+var itemCtrl = require('./itemcontrorller');
+router.get('/hello', itemCtrl.getWorld);
+
+// router.get('/hello/:foo/:bar', function(req, res) {
+//     res.json({message: 'Hello JSON', data: [
+//         req.params.foo,
+//         req.params.bar
+//     ] });
+// });
 
 router.post('/hello', function(req,res) {
     res.json({result: 'Post was sent', data: req.body});
