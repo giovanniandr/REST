58920c1439ae80d4c821f076f5945fa5ce47fc66
giovanniandr <fs_giovanni@hotmail.com>
2021-04-29 23:10:56 +0000

    Backup

--- routes.js
+++ routes.js
@@ -1,39 +1,29 @@
-//Const and express required 
 const express = require('express'),
 router = express.Router();
 
-//Controllers
-var userCtrl = require('./user-controller');
-var imageCtrl = require('./image-controller');
-var ingridientsdCtrl = require('./ingridients-controller');
+var
+// itemCtrl = require('./item-controller'),
+userCtrl = require('./user-controller');
 
-//Vars
-var multer = require("multer");
-var upload = multer({ dest: module.exports.UPLOAD_PATH});
+// router.get('/hello', itemCtrl.getWorld);
+// router.get('/hello/:foo/:bar', itemCtrl.getWorldParams);
+// router.post('/hello', itemCtrl.postWorld);
 
-//Full CRUD for users
 router.post('/users', userCtrl.createUser);
 router.get('/users', userCtrl.getUsers);
 router.get('/users/:id', userCtrl.getUser);
 router.put('/users/:id', userCtrl.updateUser);
 router.delete('/users/:id', userCtrl.deleteUser);
 
-//Full CRUD for ingridients
-router.post('/ingridients', ingridientsdCtrl.createIngridient);
-router.get('/ingridients', ingridientsdCtrl.getIngridients);
-router.get('/ingridients/:id', ingridientsdCtrl.getIngridient);
-router.put('/ingridients/:id', ingridientsdCtrl.updateIngridient);
-router.delete('/ingridients/:id', ingridientsdCtrl.deleteIngridient);
+module.exports.UPLOAD_PATH = "uploads";
 
-//Full CRUD for images
+var multer = require("multer");
+var upload = multer({ dest: module.exports.UPLOAD_PATH});
+var imageCtrl = require('./image-controller');
+
 router.post('/images', upload.single('image'), imageCtrl.uploadImage);
 router.get('/images', imageCtrl.getImages);
 router.get('/images/:id', imageCtrl.getImage);
 router.delete('/images/:id', imageCtrl.deleteImage);
 
-//Modules
-module.exports.UPLOAD_PATH = "uploads";
 module.exports = router;
-
-
-
