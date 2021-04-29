181caf5e634c2bcf7aeb95d02a7380a8fa0fa30e
giovanniandr <fs_giovanni@hotmail.com>
2021-04-29 19:22:25 +0000

    badrequesterro

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 //Constants requiring 
 const http = require('http'),
-axios = require('axios'),
 logger = require('morgan'),
 cors = require('cors'),
 express = require('express'),
 bodyParser = require('body-parser'),
-mongoose = require('mongoose');
+mongoose = require('mongoose'),
+dotenv = require("dotenv");
 
 
 var app = express();
-var port = 8000;
+var port = process.env.PORT || 8000;
+dotenv.config();
 
 app.use(bodyParser.json());
 app.use(logger('tiny'));
 app.use(require('./routes'));
-
-
 app.listen(port, function(err){
-    console.log('Listening on port:' + port);
+    console.log('Listening on port: ' + port);
 });
 
 const dbURI = "mongodb://localhost/test";
